08235fd14601febe2080d3b2dd259592aeb4ec23
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-02-06 13:37:34 +0900

    using Serverless for production

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -10,6 +10,11 @@ def home():
 def processing():
     return "<p>Processing</p>"
 
+# Sau chuyển phần này thành jsonify kết quả
 @app.route("/result")
 def result():
     return "<p>配色:</p><p>地名:</p><p>分類番号:</p><p>ひらがな:</p><p>ナンバー:</p>"
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
